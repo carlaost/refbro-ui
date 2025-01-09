e374a741ccd78a3677e867a09469dc0064ef300d
carlaost <ostmanncarla@gmail.com>
2025-01-09 17:54:06 +0100

    beta singup form

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Search from './components/pages/Search'
 import Results from './components/pages/Results'
+import { Button } from './components/ui/button'
 function App() {
 
   return (
     <Router>
-      <nav className="flex flex-row w-screen py-2 px-6 justify-start items-center">
+      <nav className="flex flex-row w-screen py-2 px-6 justify-between items-center">
         <Link to="/" className="text-black hover:text-gray-500 ">Oshima</Link>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLScFQH7VteSZsI0l36M7FSqZJ8SmOScVcWWNYhnrccMDX0K8pQ/viewform?usp=sharing" target="_blank" rel="noopener noreferrer" className="text-black hover:text-gray-500">
+          <Button>Beta Access</Button>
+        </a>
 
       </nav>
 
